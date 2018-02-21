645ec8038d62eebddb156e2457db2e4e7c3584e6
David Tran <davidtran19@yahoo.com>
2018-02-22 00:03:29 +0100

    Added neccessary js files and csv data

--- source/js/main.js
+++ source/js/main.js
@@ -3,8 +3,12 @@ queue()
 .await(draw);
 
 var dataMining;
+var map_chart;
+var map_image;
 
 function draw(error, data){
   if (error) throw error;
   dataMining = new dataMining(data);
+  map_image = new map(data);
+  map_chart = new chart();
 }
---
+++ source/js/map.js
@@ -0,0 +1,8 @@
+function convert2Coordinates(data){
+    //Function to convert coordinates in order to plot points/heatmap
+}
+
+function map(){
+    //Current CSGO map.
+    var div = "#map_image"
+}
