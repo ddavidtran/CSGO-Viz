7017e1e2737775e535fb3fdbf12505e8cca5fe78
David Tran <davidtran19@yahoo.com>
2018-02-22 15:34:22 +0100

    Progress in map.js

--- source/js/map.js
+++ source/js/map.js
@@ -2,7 +2,43 @@ function convert2Coordinates(data){
     //Function to convert coordinates in order to plot points/heatmap
 }
 
-function map(){
+function map(data){
     //Current CSGO map.
-    var div = "#map_image"
+    var div = "#map_image";
+    var width = d3.select("div").style("width"); //svg width
+    var height = d3.select("div").style("height"); //svg height
+
+
+    svg = d3.select("div").append("svg")
+            .attr("width",  width)
+            .attr("height", height);
+
+    var g = svg.append("svg");
+    
+    var img = g.append("svg:image")
+            .attr("xlink:href", "/assets/maps/de_dust2.png")
+            .style("width", width)
+            .style("height", height);
+
+    function redraw(){
+        // Extract the width and height that was computed by CSS.
+        var width = d3.select("div").style("width"); //svg width
+        var height = d3.select("div").style("height"); //svg height
+
+        // Use the extracted size to set the size of an SVG element.
+        svg
+            .attr("width", width)
+            .attr("height", height);
+        
+        img 
+            .style("width", width)
+            .style("height", height);
+    }
+
+    // Draw for the first time to initialize.
+    redraw();
+
+    // Redraw based on the new size whenever the browser window is resized.
+    window.addEventListener("resize", redraw);
+
 }
