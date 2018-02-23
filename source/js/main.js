afd5a89827daac991a15b91442d46224c0247a80
David Tran <davidtran19@yahoo.com>
2018-02-23 17:02:56 +0100

    Implemented convertCoordinates function. Parsing csv data files from main.js

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,7 @@
 queue()
-.defer(d3.csv,'source/data/mm_master.csv')
+.defer(d3.csv,'source/data/mm_master_de_dust2.csv')
+.defer(d3.csv,'source/data/mm_master_de_inferno.csv')
+.defer(d3.csv,'source/data/mm_master_de_train.csv')
 .await(draw);
 
 var dataMining;
@@ -7,9 +9,7 @@ var map_chart;
 var map_image;
 var currentMap;
 
-
-
-function draw(error, data){
+function draw(error, data1, data2, data3){
   if (error) throw error;
   
   document.getElementById("de_dust2").onclick = function(){ //If de_dust2 map is selected.
@@ -19,17 +19,17 @@ function draw(error, data){
     if(document.getElementById("canvas") != null){
       document.getElementById("canvas").remove(); //Reset canvas when changing map.
     }
-    map_image = new map(data, currentMap);
+    map_image = new map(data1, currentMap);
   }
 
-  document.getElementById("de_nuke").onclick = function(){ //If de_nuke map is selected.
+  document.getElementById("de_inferno").onclick = function(){ //If de_nuke map is selected.
     var e = window.event;
     btn = e.target || e.srcElement;
     currentMap = String(btn.id);
     if(document.getElementById("canvas") != null){
       document.getElementById("canvas").remove(); //Reset canvas when changing map.
     }
-    map_image = new map(data, currentMap);
+    map_image = new map(data2, currentMap);
   }
 
   document.getElementById("de_train").onclick = function(){ //If de_train is selected
@@ -39,9 +39,9 @@ function draw(error, data){
     if(document.getElementById("canvas") != null){
       document.getElementById("canvas").remove(); //Reset canvas when changing map.
     }
-    map_image = new map(data, currentMap);
+    map_image = new map(data3, currentMap);
   }
 
-  dataMining = new dataMining(data);
+  //dataMining = new dataMining(data);
   //map_chart = new chart();
 }
--- source/js/map.js
+++ source/js/map.js
@@ -1,17 +1,47 @@
-function convert2Coordinates(data){
-    //Function to convert coordinates in order to plot points/heatmap
-}
+/*
+md['att_pos_x'] = (md['ResX']*(md['att_pos_x']-md['StartX']))/(md['EndX']-md['StartX'])
+md['att_pos_y'] = (md['ResY']*(md['att_pos_y']-md['StartY']))/(md['EndY']-md['StartY'])
+md['vic_pos_x'] = (md['ResX']*(md['vic_pos_x']-md['StartX']))/(md['EndX']-md['StartX'])
+md['vic_pos_y'] = (md['ResY']*(md['vic_pos_y']-md['StartY']))/(md['EndY']-md['StartY'])
+*/
 
+function convertCoordinates(data, map, resX, resY){
+    var newData = data;
+    var i = 0;
+    var mapData = d3.csv("/source/data/map_data.csv", function(m){
+        if(map == "de_dust2"){ i = 0; }
+        else if(map == "de_inferno") { i = 1; }
+        else{ i = 2 }
+        newData.forEach(function(d){
+            if(m[i].map == d.map && m[i].map == map){
+                d.att_pos_x = parseFloat(d.att_pos_x);
+                d.att_pos_x = parseFloat(resX) * ((Number(d.att_pos_x) - Number(m[i].StartX))/(Number(m[i].EndX) - Number(m[i].StartX)));
+                
+                d.att_pos_y = parseFloat(d.att_pos_y);
+                d.att_pos_x = parseFloat(resY) * ((Number(d.att_pos_y) - Number(m[i].StartY))/(Number(m[i].EndY) - Number(m[i].StartY)));
 
+                d.vic_pos_x = parseFloat(d.vic_pos_x);
+                d.vic_pos_x = parseFloat(resX) * ((Number(d.vic_pos_x) - Number(m[i].StartX))/(Number(m[i].EndX) - Number(m[i].StartX)));
+
+                d.vic_pos_y = parseFloat(d.vic_pos_y);
+                d.vic_pos_y = parseFloat(resY) * ((Number(d.vic_pos_y) - Number(m[i].StartY))/(Number(m[i].EndY) - Number(m[i].StartY)));
+            }
+        })
+    });
+
+    return newData
+  }
+  
 
 function map(data, map){
     //Current CSGO map
     var currentMap = map;
-    
     var div = "#map_image";
     var width = d3.select("div").style("width"); //svg width
     var height = d3.select("div").style("height"); //svg height
 
+    var newData = convertCoordinates(data,map,width,height);
+    
     svg = d3.select("div").append("svg")
             .attr("id", "canvas")
             .attr("width",  width)
